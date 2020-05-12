58fdce824936e1c5f868fe4ffa2556939b5a7a9e
James.Schlader <jamesschlader@yahoo.com>
2020-05-12 13:55:24 -0600

    added Operator class

--- src/main/java/com/brianandjim/fourddata/entity/models/ValueFunction.java
+++ src/main/java/com/brianandjim/fourddata/entity/models/ValueFunction.java
@@ -1,5 +1,7 @@
 package com.brianandjim.fourddata.entity.models;
 
+import com.brianandjim.fourddata.services.utils.Operator;
+
 public interface ValueFunction {
-    Object getValue(NodeValueSpace xSpace, NodeValueSpace ySpace, char operator);
+    Object getValue(NodeValueSpace xSpace, NodeValueSpace ySpace, Operator operator);
 }
