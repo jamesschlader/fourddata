ec2e0048ae87ad5413eb27126857a2ac8be14388
bjacks801 <def_python@icloud.com>
2020-05-13 07:03:24 -0600

    adding graphql

---
+++ src/main/java/com/brianandjim/fourddata/services/universe/UniverseService.java
@@ -0,0 +1,34 @@
+package com.brianandjim.fourddata.services.universe;
+
+import com.brianandjim.fourddata.entity.dao.UniverseDao;
+import com.brianandjim.fourddata.entity.dtos.UniverseDTO;
+import com.brianandjim.fourddata.entity.models.Universe;
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@GraphQLApi
+public class UniverseService {
+
+    private final UniverseDao universeDao;
+
+    public UniverseService(UniverseDao universeDao) {
+        this.universeDao = universeDao;
+    }
+
+    @GraphQLQuery(name="universes")
+    public List<Universe> getAllUniverses(){
+        return universeDao.findAll();
+    }
+
+    @GraphQLMutation(name="saveUniverse")
+    public Universe saveUniverse(@GraphQLArgument(name="newUniverse")UniverseDTO universeDTO){
+        return universeDao.saveAndFlush(new Universe(universeDTO));
+    }
+}
